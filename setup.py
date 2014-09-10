dbf168ba65c77bd61ed6db38717db3fbe6b0089f
Robert Grant <rgrant@mdi.us.com>
2014-09-10 17:02:02 -0400

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import sys
+
+setup(
+    name="OTP Tunnel",
+    version="0.1.0dev",
+    author="Robert Graham",
+    author_email="rpgraham84@gmail.com",
+    packages=["otpt"],
+    scripts=["bin/otptunnel"],
+    url="http://pypi.python.org/pypi/OTPTunnel",
+    license="LICENSE",
+    description="Create an enrypted tunnel interface using a one-time pad.",
+    long_description=open("README.md").read()
+)
