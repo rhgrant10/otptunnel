d08afdeec52e86d08737aa42b7baba2867d7c86a
Robert Graham <rpgraham84@gmail.com>
2014-09-12 16:41:27 -0400

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 import sys
 
 setup(
-    name="OTP Tunnel",
+    name="OTPTunnel",
     version="0.1.0",
     author="Robert Graham",
     author_email="rpgraham84@gmail.com",
-    packages=["otpt"],
+    packages=["otpt", "otpt.tests"],
     scripts=["bin/otptunnel"],
-    url="http://pypi.python.org/pypi/OTPTunnel",
     license="LICENSE",
-    description="Create an enrypted tunnel interface using a one-time pad.",
+    description="Create an encrypted tunnel interface using a one-time pad.",
     long_description=open("README.md").read()
 )
